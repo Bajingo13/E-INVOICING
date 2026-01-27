c48b5d1615d385f638e11a2fac2a6e42554ad0f6
Bajingo13 <teesoii12@gmail.com>
2026-01-27 08:50:46 +0800

    Db.js changed

--- helpers/db.js
+++ helpers/db.js
@@ -1,18 +1,13 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+  uri: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  },
   waitForConnections: true,
   connectionLimit: 10,
+  connectTimeout: 15000
 });
 
-async function getConn() {
-  return await pool.getConnection();
-}
-
-const asyncHandler = fn => (req, res, next) => Promise.resolve(fn(req, res, next)).catch(next);
-
-module.exports = { getConn, asyncHandler };
+module.exports = { pool };
