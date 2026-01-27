06124416b8e54f9d7512f3b4e373aa673f4b1aeb
Bajingo13 <teesoii12@gmail.com>
2026-01-27 09:25:42 +0800

    db.js

--- helpers/db.js
+++ helpers/db.js
@@ -3,7 +3,7 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  uri: process.env.MYSQL_URL,   // <-- change this
+  uri: process.env.DATABASE_URL,   // <-- FIXED
   ssl: { rejectUnauthorized: false },
   waitForConnections: true,
   connectionLimit: 10,
