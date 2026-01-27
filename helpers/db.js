2d49a946789e2d1c27ad14b6016e6df888a9f782
Bajingo13 <teesoii12@gmail.com>
2026-01-27 09:11:07 +0800

    deleted db folder

--- helpers/db.js
+++ helpers/db.js
@@ -3,10 +3,8 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  uri: process.env.DATABASE_URL,   // <-- correct variable
-  ssl: {
-    rejectUnauthorized: false
-  },
+  uri: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
   waitForConnections: true,
   connectionLimit: 10,
   connectTimeout: 15000
